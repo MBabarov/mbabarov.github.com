e724e8a619991ffd47381fdbdb2e50b9a78e2cf1
Babarov Maxim <mbabarov@gmail.com>
2014-05-23 18:31:19 +0300

    main menu

--- js/controllers.js
+++ js/controllers.js
@@ -13,14 +13,22 @@ angular.module('myApp.controllers', [])
     $scope.menus =[
     {
         title: "Dashboard",
+        icon:"home",
+        action: "#",
+        active: false
+    },
+    {
+        title: "Inbox",
+        icon:"inbox",
         action: "#",
         active: false
     },
     {
         title: "Graphs",
+        icon:"bar-chart-o",
         action: "#",
         active: false,
-        menus: [
+        submenu: [
             {
                 title: "Flot Chart",
                 action: "#/flot"
@@ -37,9 +45,10 @@ angular.module('myApp.controllers', [])
     },
     {
         title: "Tables",
+        icon: "table",
         action: "#",
         active: false,
-        menus: [
+        submenu: [
             {
                 title: "Normal Tables",
                 action: "#/table"
@@ -52,9 +61,10 @@ angular.module('myApp.controllers', [])
     },
     {
         title: "Forms",
+        icon:"pencil-square-o",
         action: "#",
         id: 4,
-        menus: [
+        submenu: [
             {
                 title: "Smart Form Elements",
                 action: "#/form-elements"
@@ -91,9 +101,10 @@ angular.module('myApp.controllers', [])
     },
     {
         title: "UI Elements",
+        icon: "desktop",
         action: "#",
         active: false,
-        menus: [
+        submenu: [
             {
                 title: "General Elements",
                 action: "#/general-elements"
@@ -105,7 +116,7 @@ angular.module('myApp.controllers', [])
             {
                 title: "Icons",
                 action: "#",
-                menus: [
+                subsubmenu: [
                     {
                         title: 'Font Awesome',
                         action: 'fa'
@@ -133,9 +144,161 @@ angular.module('myApp.controllers', [])
                 action: "#/jqui"
             }
         ]
+    },
+    {
+        title: "6 Level Navigation",
+        icon: "folder-open",
+        action: "#",
+        active: false,
+        submenu: [
+            {
+               title: "2nd Level",
+               action: "#/general-elements",
+               subsubmenu: [
+                    {
+                       title: "3ed Level",
+                       action: "#/general-elements",
+                       subsubsubmenu: [
+                            {
+                               title: "File",
+                               action: "#/general-elements"
+                            },
+                            {
+                               title: "4th Level",
+                               action: "#/general-elements"
+                            }
+                       ]
+                    },
+                    {
+                       title: "Profile",
+                       action: "#/general-elements"
+                    }
+               ]
+            },
+            {
+               title: "Folder",
+               action: "#/general-elements",
+               subsubmenu: [
+                    {
+                       title: "3ed Level",
+                       action: "#/general-elements",
+                       subsubsubmenu: [
+                            {
+                               title: "File",
+                               action: "#/general-elements"
+                            },
+                            {
+                               title: "File",
+                               action: "#/general-elements"
+                            }
+                       ]
+                    }
+               ]
+            }
+        ]
+    },
+    {
+        title: "Calendar",
+        icon: "calendar",
+        action: "#",
+        active: false
+    },
+    {
+        title: "Widgets",
+        icon: "list-alt",
+        action: "#",
+        active: false
+    },
+    {
+        title: "Gallery",
+        icon: "picture-o",
+        action: "#",
+        active: false
+    },
+    {
+        title: "Google Map Skins",
+        icon: "map-marker",
+        action: "#",
+        active: false
+    },
+    {
+        title: "Miscellaneous",
+        icon: "windows",
+        action: "#",
+        active: false,
+        submenu: [
+            {
+                title: "Typography",
+                action: "#/general-elements"
+            },
+            {
+                title: "Pricing Tables",
+                action: "#/general-elements"
+            },
+            {
+                title: "Invoice",
+                action: "#/general-elements"
+            },
+            {
+                title: "Login",
+                action: "#/general-elements"
+            },
+            {
+                title: "Register",
+                action: "#/general-elements"
+            },
+            {
+                title: "Locked Screen",
+                action: "#/general-elements"
+            },
+            {
+                title: "Error 404",
+                action: "#/general-elements"
+            },
+            {
+                title: "Error 500",
+                action: "#/general-elements"
+            },
+            {
+                title: "Blank Page",
+                action: "#/general-elements"
+            },
+            {
+                title: "Email Template",
+                action: "#/general-elements"
+            },
+            {
+                title: "Search Page",
+                action: "#/general-elements"
+            },
+            {
+                title: "CK Editor",
+                action: "#/general-elements"
+            }
+        ]
+    },
+    {
+        title: "Other Pages",
+        icon: "file",
+        action: "#",
+        active: false,
+        submenus: [
+            {
+               title: "Forum Layout",
+               action: "#/general-elements"
+            },
+            {
+               title: "Profile",
+               action: "#/general-elements"
+            },
+            {
+               title: "Timeline",
+               action: "#/general-elements"
+            }
+        ]
     }
     ];
-    $scope.setActive = function (active) {
+    $scope.setActive = function () {
         $scope.currentElem=this;
 
         if(!!$scope.oldElement && $scope.currentElem!=$scope.oldElement){
